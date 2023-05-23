5ab5b599b966669646a5340db90f9975f49ac6b3
neurothrone <54468049+neurothrone@users.noreply.github.com>
2023-05-23 20:51:34 +0200

    clean up

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,9 @@
 import 'package:flutter/material.dart';
 
-import 'home_screen.dart';
+import 'app.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "Neurothrone",
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const HomeScreen(),
-    );
-  }
+  runApp(
+    const AppMain(),
+  );
 }
