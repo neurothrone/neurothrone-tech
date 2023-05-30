df4c16a6bfbc09fe7439cb234d798ff25950a488
neurothrone <54468049+neurothrone@users.noreply.github.com>
2023-05-30 02:54:47 +0200

    configure google fonts

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,20 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'package:google_fonts/google_fonts.dart';
 
 import 'app.dart';
 
-void main() {
+Future<void> main() async {
+  // Disable HTTP fetching: fonts are available in assets
+  GoogleFonts.config.allowRuntimeFetching = false;
+
+  LicenseRegistry.addLicense(() async* {
+    final license = await rootBundle.loadString("assets/fonts/LICENSE.txt");
+    yield LicenseEntryWithLineBreaks(['google_fonts'], license);
+  });
+
   runApp(
     const AppMain(),
   );
