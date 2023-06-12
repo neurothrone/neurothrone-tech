db29c2ae08bddb65192d1dc3b6b5cf3d03305ab2
neurothrone <54468049+neurothrone@users.noreply.github.com>
2023-06-12 23:23:01 +0200

    update import

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:neurothrone_tech/app.dart';
+import 'package:neurothrone_tech/src/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
