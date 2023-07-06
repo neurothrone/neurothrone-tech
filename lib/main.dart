6fc5ca9e113bae593f1f3ba4900984892a10d101
neurothrone <54468049+neurothrone@users.noreply.github.com>
2023-07-06 07:29:31 +0200

    fix path url strategy

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 import 'package:google_fonts/google_fonts.dart';
 
 import 'src/app.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Turns off the # in the URLs on the web
+  usePathUrlStrategy();
+
   // Disable HTTP fetching: fonts are available in assets
-  GoogleFonts.config.allowRuntimeFetching = false;
+  // GoogleFonts.config.allowRuntimeFetching = false;
 
   LicenseRegistry.addLicense(() async* {
     final license = await rootBundle.loadString("assets/fonts/LICENSE.txt");
