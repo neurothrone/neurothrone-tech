ef2322ca6beb3b76a6da0f85e950f357b7eb72b7
neurothrone <54468049+neurothrone@users.noreply.github.com>
2025-07-18 06:09:10 +0200

    add HomePage and ProjectsPage widgets for navigation

---
+++ lib/src/features/projects/ui/pages/projects_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ProjectsPage extends StatelessWidget {
+  const ProjectsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("Projects"),
+      ),
+    );
+  }
+}
