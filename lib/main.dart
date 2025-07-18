5d60e8901d0d99a535b9757aef3158c5c25821b7
neurothrone <54468049+neurothrone@users.noreply.github.com>
2025-07-18 06:09:00 +0200

    wrap MainApp with ProviderScope and enable path URL strategy

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
+
 import 'src/app.dart';
 
 void main() {
+  usePathUrlStrategy();
+
   runApp(
-    const MainApp(),
+    const ProviderScope(
+      child: MainApp(),
+    ),
   );
 }
