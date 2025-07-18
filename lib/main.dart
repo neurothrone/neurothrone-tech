15231e0f79de9e5ec28f43c16513c8946ecaf7a2
neurothrone <54468049+neurothrone@users.noreply.github.com>
2025-07-18 12:17:43 +0200

    remove URL strategy usage in main.dart for compatibility

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_web_plugins/url_strategy.dart';
+// import 'package:flutter_web_plugins/url_strategy.dart';
 
 import 'src/app.dart';
 
 void main() {
-  usePathUrlStrategy();
+  // usePathUrlStrategy();
 
   runApp(
     const ProviderScope(
