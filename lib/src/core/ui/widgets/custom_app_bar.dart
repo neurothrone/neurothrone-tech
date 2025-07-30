6af43705b034d5777a3a1e38de19ad7b37b117be
neurothrone <54468049+neurothrone@users.noreply.github.com>
2025-07-31 00:20:41 +0200

    enhance CustomAppBar with actions, bottom widget, and divider option

--- lib/src/core/ui/widgets/custom_app_bar.dart
+++ lib/src/core/ui/widgets/custom_app_bar.dart
@@ -4,18 +4,52 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   const CustomAppBar({
     super.key,
     required this.title,
+    this.actions,
+    this.bottom,
+    this.showDivider = true,
   });
 
   final String title;
+  final List<Widget>? actions;
+  final PreferredSizeWidget? bottom;
+  final bool showDivider;
 
   @override
   Widget build(BuildContext context) {
+    PreferredSizeWidget? wrappedBottom = bottom;
+    if (bottom != null && showDivider) {
+      wrappedBottom = PreferredSize(
+        preferredSize: bottom!.preferredSize,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const Divider(
+              color: Colors.white38,
+              height: 0.5,
+              // thickness: 1,
+            ),
+            bottom!,
+          ],
+        ),
+      );
+    }
+
     return AppBar(
       centerTitle: true,
-      title: Text(title),
+      title: Text(
+        title,
+        style: const TextStyle(color: Colors.white),
+      ),
+      backgroundColor: Colors.deepPurpleAccent,
+      iconTheme: const IconThemeData(color: Colors.white),
+      actionsIconTheme: const IconThemeData(color: Colors.white),
+      actions: actions,
+      actionsPadding: const EdgeInsets.only(right: 6.0),
+      bottom: wrappedBottom,
     );
   }
 
   @override
-  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+  Size get preferredSize =>
+      Size.fromHeight(kToolbarHeight + (bottom?.preferredSize.height ?? 0));
 }
--- lib/src/core/ui/widgets/widgets.dart
+++ lib/src/core/ui/widgets/widgets.dart
@@ -1,3 +1,4 @@
+export 'app_drawer.dart';
 export 'app_scaffold.dart';
 export 'custom_app_bar.dart';
 export 'custom_navigation_bar.dart';
