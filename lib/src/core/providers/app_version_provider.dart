9f13125806791515f5f86b5bbdea4aa5c2be1274
neurothrone <54468049+neurothrone@users.noreply.github.com>
2025-08-01 09:55:40 +0200

    capitalize app name in app version provider

--- lib/src/core/providers/app_version_provider.dart
+++ lib/src/core/providers/app_version_provider.dart
@@ -1,8 +1,10 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 
+import '../utils/utils.dart';
+
 final appVersionProvider = FutureProvider<String>((ref) async {
   await Future.delayed(const Duration(seconds: 2));
   final info = await PackageInfo.fromPlatform();
-  return "${info.appName} (${info.version})";
+  return "${info.appName.capitalized} (${info.version})";
 });
