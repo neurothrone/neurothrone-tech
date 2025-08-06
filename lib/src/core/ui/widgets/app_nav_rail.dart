228aeaaba721efaf9fc29ff4122b641a4d6616fb
neurothrone <54468049+neurothrone@users.noreply.github.com>
2025-08-06 09:29:14 +0200

    feat: enhance AppNavRail with container styling and border decoration

--- lib/src/core/ui/widgets/app_nav_rail.dart
+++ lib/src/core/ui/widgets/app_nav_rail.dart
@@ -4,6 +4,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
 import '../../navigation/navigation.dart';
+import '../theme/theme.dart';
 
 class AppNavRail extends ConsumerWidget {
   const AppNavRail({super.key});
@@ -12,37 +13,43 @@ class AppNavRail extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final activeRoute = ref.watch(currentRouteProvider);
 
-    return NavigationRail(
-      groupAlignment: -1.0,
-      backgroundColor: Colors.deepPurpleAccent,
-      selectedIndex: getNavRailIndex(activeRoute),
-      onDestinationSelected: (int index) {
-        final route = AppRoute.fromIndex(index);
-        context.goNamed(route.name);
-      },
-      labelType: NavigationRailLabelType.all,
-      destinations: const [
-        NavigationRailDestination(
-          label: Text("Home"),
-          icon: Icon(Icons.home_outlined),
-          selectedIcon: Icon(Icons.home_rounded),
+    return Container(
+      decoration: BoxDecoration(
+        border: Border(
+          right: borderDividerStyle,
         ),
-        NavigationRailDestination(
-          label: Text("Projects"),
-          icon: Icon(Icons.folder_outlined),
-          selectedIcon: Icon(Icons.folder_rounded),
-        ),
-        NavigationRailDestination(
-          label: Text("Settings"),
-          icon: Icon(Icons.settings_outlined),
-          selectedIcon: Icon(Icons.settings_rounded),
-        ),
-        NavigationRailDestination(
-          label: Text("Time"),
-          icon: Icon(Icons.insert_chart_outlined_rounded),
-          selectedIcon: Icon(Icons.insert_chart_rounded),
-        ),
-      ],
+      ),
+      child: NavigationRail(
+        groupAlignment: -1.0,
+        selectedIndex: getNavRailIndex(activeRoute),
+        onDestinationSelected: (int index) {
+          final route = AppRoute.fromIndex(index);
+          context.goNamed(route.name);
+        },
+        labelType: NavigationRailLabelType.all,
+        destinations: const [
+          NavigationRailDestination(
+            label: Text("Home"),
+            icon: Icon(Icons.home_outlined),
+            selectedIcon: Icon(Icons.home_rounded),
+          ),
+          NavigationRailDestination(
+            label: Text("Projects"),
+            icon: Icon(Icons.folder_outlined),
+            selectedIcon: Icon(Icons.folder_rounded),
+          ),
+          NavigationRailDestination(
+            label: Text("Settings"),
+            icon: Icon(Icons.settings_outlined),
+            selectedIcon: Icon(Icons.settings_rounded),
+          ),
+          NavigationRailDestination(
+            label: Text("Time"),
+            icon: Icon(Icons.insert_chart_outlined_rounded),
+            selectedIcon: Icon(Icons.insert_chart_rounded),
+          ),
+        ],
+      ),
     );
   }
 }
