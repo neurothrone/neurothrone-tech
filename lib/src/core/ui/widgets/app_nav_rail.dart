9c00c9df40afbc01314acbcb7e510962a890e316
neurothrone <54468049+neurothrone@users.noreply.github.com>
2025-08-07 21:22:26 +0200

    feat: enhance navigation rail with tooltip support for icons

--- lib/src/core/ui/widgets/app_nav_rail.dart
+++ lib/src/core/ui/widgets/app_nav_rail.dart
@@ -16,59 +16,113 @@ class AppNavRail extends ConsumerWidget {
     final activeRoute = ref.watch(currentRouteProvider);
     final isExpanded = ref.watch(navRailExpandedProvider);
 
+    Widget buildRailIcon({
+      required bool showTooltip,
+      required String tooltip,
+      required IconData icon,
+    }) {
+      final iconWidget = Icon(icon);
+      return showTooltip
+          ? Tooltip(
+              message: tooltip,
+              waitDuration: Duration(milliseconds: 0),
+              child: iconWidget,
+            )
+          : iconWidget;
+    }
+
     return Container(
       width: isExpanded ? 100 : 72,
       decoration: BoxDecoration(
         border: Border(right: kBorderSide),
         color: kPanelDarkColor,
       ),
-      child: Column(
-        children: [
-          Expanded(
-            child: NavigationRail(
-              groupAlignment: -1.0,
-              selectedIndex: getNavRailIndex(activeRoute),
-              onDestinationSelected: (int index) {
-                final route = AppRoute.fromIndex(index);
-                context.goNamed(route.name);
-              },
-              labelType: isExpanded
-                  ? NavigationRailLabelType.all
-                  : NavigationRailLabelType.none,
-              destinations: const [
-                NavigationRailDestination(
-                  label: Text("Home"),
-                  icon: Icon(Icons.home_outlined),
-                  selectedIcon: Icon(Icons.home_rounded),
-                ),
-                NavigationRailDestination(
-                  label: Text("Projects"),
-                  icon: Icon(Icons.folder_outlined),
-                  selectedIcon: Icon(Icons.folder_rounded),
-                ),
-                NavigationRailDestination(
-                  label: Text("Sleep"),
-                  icon: Icon(Icons.bedtime_outlined),
-                  selectedIcon: Icon(Icons.bedtime_rounded),
-                ),
-                NavigationRailDestination(
-                  label: Text("Settings"),
-                  icon: Icon(Icons.settings_outlined),
-                  selectedIcon: Icon(Icons.settings_rounded),
-                ),
-              ],
+      child: NavigationRail(
+        selectedIndex: getNavRailIndex(activeRoute),
+        onDestinationSelected: (int index) {
+          final route = AppRoute.fromIndex(index);
+          context.goNamed(route.name);
+        },
+        leading: IconButton(
+          tooltip: isExpanded ? "Collapse navigation" : "Expand navigation",
+          icon: Icon(
+            isExpanded
+                ? Icons.keyboard_double_arrow_left_rounded
+                : Icons.keyboard_double_arrow_right_rounded,
+          ),
+          onPressed: () {
+            ref.read(navRailExpandedProvider.notifier).state = !isExpanded;
+          },
+        ),
+        labelType: isExpanded
+            ? NavigationRailLabelType.all
+            : NavigationRailLabelType.none,
+        groupAlignment: -1.0,
+        destinations: [
+          NavigationRailDestination(
+            label: Text("Home"),
+            icon: buildRailIcon(
+              showTooltip: !isExpanded,
+              tooltip: "Home",
+              icon: Icons.home_outlined,
+            ),
+            selectedIcon: buildRailIcon(
+              showTooltip: !isExpanded,
+              tooltip: "Home",
+              icon: Icons.home_rounded,
             ),
           ),
-          IconButton(
-            tooltip: isExpanded ? "Collapse navigation" : "Expand navigation",
-            icon: Icon(isExpanded ? Icons.chevron_left : Icons.chevron_right),
-            onPressed: () {
-              ref.read(navRailExpandedProvider.notifier).state = !isExpanded;
-            },
+          NavigationRailDestination(
+            label: Text("Projects"),
+            icon: buildRailIcon(
+              showTooltip: !isExpanded,
+              tooltip: "Projects",
+              icon: Icons.folder_outlined,
+            ),
+            selectedIcon: buildRailIcon(
+              showTooltip: !isExpanded,
+              tooltip: "Projects",
+              icon: Icons.folder_rounded,
+            ),
           ),
-          const SizedBox(height: 10.0),
+          NavigationRailDestination(
+            label: Text("Sleep"),
+            icon: buildRailIcon(
+              showTooltip: !isExpanded,
+              tooltip: "Sleep",
+              icon: Icons.bedtime_outlined,
+            ),
+            selectedIcon: buildRailIcon(
+              showTooltip: !isExpanded,
+              tooltip: "Sleep",
+              icon: Icons.bedtime_rounded,
+            ),
+          ),
+          NavigationRailDestination(
+            label: Text("Settings"),
+            icon: buildRailIcon(
+              showTooltip: !isExpanded,
+              tooltip: "Settings",
+              icon: Icons.settings_outlined,
+            ),
+            selectedIcon: buildRailIcon(
+              showTooltip: !isExpanded,
+              tooltip: "Settings",
+              icon: Icons.settings_rounded,
+            ),
+          ),
         ],
       ),
     );
   }
 }
+
+// class NavRailItem extends NavigationRailDestination {
+//   const NavRailItem({
+//     this.showTooltip = true,
+//     required super.icon,
+//     required super.label,
+//   });
+//
+//   final bool showTooltip;
+// }
