db5c9d86027f1a47742186ea0cb6e735a0ff75ca
neurothrone <54468049+neurothrone@users.noreply.github.com>
2025-08-09 14:19:15 +0200

    feat: add sleepListNotifierProvider and update sleepTabNotifierProvider for improved state management

--- lib/src/features/sleep/sections/this_week/state/providers.dart
+++ lib/src/features/sleep/sections/this_week/state/providers.dart
@@ -1,7 +1,19 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../../../shared/services/services.dart';
+import '../../../shared/state/providers.dart';
 import '../domain/domain.dart';
+import 'sleep_list_notifier.dart';
+import 'sleep_list_state.dart';
 
 final weekViewStateProvider = StateProvider<WeekView>(
   (ref) => WeekView.thisWeek,
 );
+
+final sleepListNotifierProvider =
+    StateNotifierProvider<SleepListNotifier, AsyncValue<SleepListState>>((
+      ref,
+    ) {
+      final SleepNetworkService service = ref.read(sleepServiceProvider);
+      return SleepListNotifier(service: service);
+    });
--- lib/src/features/sleep/shared/state/providers.dart
+++ lib/src/features/sleep/shared/state/providers.dart
@@ -1,28 +1,19 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../../../core/setup/dependencies.dart';
-import '../../sections/this_week/state/sleep_list_notifier.dart';
-import '../../sections/this_week/state/sleep_list_state.dart';
 import '../domain/domain.dart';
 import '../services/services.dart';
 import 'sleep_tab_notifier.dart';
 
-final sleepTabNotifierProvider = StateNotifierProvider<SleepTabNotifier, SleepTab>((
-  ref,
-) {
-  return SleepTabNotifier(
-    service: serviceLocator<SleepPreferencesService>(),
-  );
-});
+final sleepTabNotifierProvider =
+    StateNotifierProvider<SleepTabNotifier, SleepTab>((
+      ref,
+    ) {
+      return SleepTabNotifier(
+        service: serviceLocator<SleepPreferencesService>(),
+      );
+    });
 
 final sleepServiceProvider = Provider<SleepNetworkService>((ref) {
   return serviceLocator<SleepNetworkService>();
 });
-
-final sleepListNotifierProvider =
-    StateNotifierProvider<SleepListNotifier, AsyncValue<SleepListState>>((
-      ref,
-    ) {
-      final SleepNetworkService service = ref.read(sleepServiceProvider);
-      return SleepListNotifier(service: service);
-    });
