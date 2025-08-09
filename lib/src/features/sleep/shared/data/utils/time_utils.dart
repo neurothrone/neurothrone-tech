4c51e2caf7c3f7a0f10bef89d0335c9e37018395
neurothrone <54468049+neurothrone@users.noreply.github.com>
2025-08-09 17:41:43 +0200

    feat: update format of duration string to improve readability

--- lib/src/features/sleep/shared/data/utils/time_utils.dart
+++ lib/src/features/sleep/shared/data/utils/time_utils.dart
@@ -1,5 +1,5 @@
 String formatDurationFromSeconds(int seconds) {
   final hours = seconds ~/ 3600;
   final minutes = (seconds % 3600) ~/ 60;
-  return "$hours h ${minutes.toString().padLeft(2, '0')} min";
+  return "${hours}h ${minutes.toString().padLeft(2, "0")}m";
 }
