bb87de5054648d2717a45bfa4e6c4f2776e1bbb4
neurothrone <54468049+neurothrone@users.noreply.github.com>
2025-08-09 14:47:50 +0200

    feat: implement sleep log loading and display for the current week

--- lib/src/features/sleep/sections/this_week/sleep_this_week_view.dart
+++ lib/src/features/sleep/sections/this_week/sleep_this_week_view.dart
@@ -2,8 +2,9 @@ import 'package:flutter/material.dart';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import 'domain/domain.dart';
+import '../../shared/data/models/models.dart';
 import 'state/providers.dart';
+import 'state/sleep_list_state.dart';
 
 class SleepThisWeekView extends ConsumerWidget {
   const SleepThisWeekView({super.key});
@@ -11,6 +12,9 @@ class SleepThisWeekView extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final weekView = ref.watch(weekViewStateProvider);
+    final sleepLogsAsyncState = ref.watch(
+      sleepListNotifierProvider(weekView),
+    );
 
     return AnimatedSwitcher(
       duration: const Duration(milliseconds: 300),
@@ -22,15 +26,45 @@ class SleepThisWeekView extends ConsumerWidget {
           child: child,
         );
       },
-      child: weekView == WeekView.thisWeek
-          ? const Text(
-              key: ValueKey(WeekView.thisWeek),
-              "This Week's Sleep Data",
-            )
-          : const Text(
-              key: ValueKey(WeekView.lastWeek),
-              "Last Week's Sleep Data",
-            ),
+      child: sleepLogsAsyncState.when(
+        data: (SleepListState state) {
+          if (state.logs.isEmpty) {
+            return const Center(
+              child: Text("No sleep logs available for this week."),
+            );
+          }
+          return SleepLogList(logs: state.logs);
+        },
+        loading: () => const Center(child: CircularProgressIndicator()),
+        error: (error, stackTrace) => Center(
+          child: Text("Error loading sleep logs: $error"),
+        ),
+      ),
+    );
+  }
+}
+
+class SleepLogList extends StatelessWidget {
+  const SleepLogList({
+    super.key,
+    required this.logs,
+  });
+
+  final List<SleepLog> logs;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: logs.length,
+      itemBuilder: (context, index) {
+        final log = logs[index];
+        return ListTile(
+          title: Text(log.wokeUpAt.toIso8601String()),
+          subtitle: Text(
+            "Duration: ${log.duration.inHours}h ${log.duration.inMinutes.remainder(60)}m",
+          ),
+        );
+      },
     );
   }
 }
